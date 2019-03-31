c891e4d1c8f655a0778d686b69c5a41be33fea38
zhangxp0111 <zhangxp0111@aliyun.com>
2019-03-31 18:07:18 +0800

    updata

---
+++ src/main/java/HashMapLearn.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class HashMapLearn {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "zhans");
+        map.put(17, "zhans");
+        map.put(33, "zhans");
+
+
+
+    }
+}
